f7738632b163e1fa4924dd57f38c105c5618a160
ilhammrtaa11 <m.ilhammartas11@gmail.com>
2026-01-21 20:41:48 +0700

    menambahkan laman home dari login user

---
+++ auth/home.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["user_id"])){
+    header("Location: auth/login.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+
+    <h2>Selamat Datang, <?php echo $_SESSION["username"]; ?></h2>
+
+    <a href="auth/logout.php">Logout</a>
+
+</body>
+</html>
